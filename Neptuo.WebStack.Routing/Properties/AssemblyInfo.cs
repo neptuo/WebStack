edd2fda99b95ff17dab3db3ca3339b8c37ae06c6
Marek Fišera <maraf@users.noreply.github.com>
2014-08-22 17:46:57 +0000

    Initial project structure.

---
+++ Neptuo.WebStack.Routing/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using Neptuo.WebStack.Routing;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Neptuo.WebStack.Routing")]
+[assembly: AssemblyDescription("Routing for web stuff by Neptuo.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Neptuo")]
+[assembly: AssemblyProduct("Neptuo.WebStack.Routing")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a5e88562-a74b-4cee-984a-bc3a5611937f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(VersionInfo.Version)]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version)]
+[assembly: AssemblyFileVersion(VersionInfo.Version)]
