3b3a251eab0131a14b9563ffa6b80e98b025918d
Marek Fišera <maraf@users.noreply.github.com>
2014-08-23 07:13:57 +0000

    Some basic http stuff.

---
+++ Neptuo.WebStack/Http/IHttpContext.cs
@@ -0,0 +1,37 @@
+﻿using Neptuo.WebStack.Http.MediaTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.WebStack.Http
+{
+    /// <summary>
+    /// Describes whole Http context.
+    /// </summary>
+    public interface IHttpContext
+    {
+        /// <summary>
+        /// Http request.
+        /// </summary>
+        IHttpRequest Request { get; }
+
+        /// <summary>
+        /// Http response.
+        /// </summary>
+        IHttpResponse Response { get; }
+
+        /// <summary>
+        /// Collection of custom values.
+        /// </summary>
+        IDictionary<string, string> Values { get; }
+
+        /// <summary>
+        /// Resolves url starting with ~/...
+        /// </summary>
+        /// <param name="appRelativeUrl">Application relative url.</param>
+        /// <returns>Absolute url.</returns>
+        string ResolveUrl(string appRelativeUrl);
+    }
+}
---
+++ Neptuo.WebStack/Pipelines/IPipeline.cs
@@ -0,0 +1,21 @@
+﻿using Neptuo.WebStack.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.WebStack.Pipelines
+{
+    /// <summary>
+    /// Defines invokable pipeline for processing complete Http request with response.
+    /// </summary>
+    public interface IPipeline
+    {
+        /// <summary>
+        /// Process <paramref name="httpContext"/>.
+        /// </summary>
+        /// <param name="httpContext">Current Http context.</param>
+        void Invoke(IHttpContext httpContext);
+    }
+}
