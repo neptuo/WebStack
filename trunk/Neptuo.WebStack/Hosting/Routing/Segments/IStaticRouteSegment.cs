3b734591edb81cb7f1f17c8e0323f4ce15a09292
Mara <Mara@d3b2a240-04b9-5443-bd83-0f4699e8c419>
2014-09-13 00:20:36 +0000

    Comments on StaticRouteSegment

---
+++ trunk/Neptuo.WebStack/Hosting/Routing/Segments/IStaticRouteSegment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.WebStack.Hosting.Routing.Segments
+{
+    public interface IStaticRouteSegment
+    {
+        string UrlPart { get; set; }
+        List<RouteSegment> Children { get; protected set; }
+    }
+}
