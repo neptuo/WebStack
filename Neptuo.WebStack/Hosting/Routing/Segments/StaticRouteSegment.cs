73bc5c5d6e6d2e69b784cdfc10b1b5e7da724122
Marek Fišera <maraf@users.noreply.github.com>
2014-09-13 00:20:36 +0000

    Comments on StaticRouteSegment

--- Neptuo.WebStack/Hosting/Routing/Segments/StaticRouteSegment.cs
+++ Neptuo.WebStack/Hosting/Routing/Segments/StaticRouteSegment.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Neptuo.WebStack.Hosting.Routing.Segments
 {
-    public class StaticRouteSegment : RouteSegment
+    public class StaticRouteSegment : RouteSegment, IStaticRouteSegment
     {
         public IPipelineFactory PipelineFactory { get; private set; }
 
@@ -23,6 +23,12 @@ namespace Neptuo.WebStack.Hosting.Routing.Segments
             PipelineFactory = pipelineFactory;
         }
 
+        /// <summary>
+        /// Returns string after matching with <see cref="UrlPart"/>.
+        /// Returns <c>null</c> when <paramref name="url"/> doesn't start with <see cref="UrlPart"/>.
+        /// </summary>
+        /// <param name="url">Url to compare and shrink shared chars.</param>
+        /// <returns><paramref name="url"/> without shared first-n chars with <see cref="UrlPart"/>; <c>false</c> when <paramref name="url"/> doesn't start with <see cref="UrlPart"/>.</returns>
         private string TryMatchUrlPart(string url)
         {
             if (url.StartsWith(UrlPart))
@@ -59,48 +65,63 @@ namespace Neptuo.WebStack.Hosting.Routing.Segments
 
         public override void IncludeUrl(string url, IPipelineFactory pipelineFactory)
         {
+            // Ignore inlcude request when 'this' segment is not matched.
             url = TryMatchUrlPart(url);
             if (url != null)
             {
+                // When no 'new' url to append to this segment, only update pipeline.
                 if (url.Length == 0)
                 {
                     PipelineFactory = pipelineFactory;
                     return;
                 }
 
+                // Only for Static route segments.
+                // Walk through child segments and try to find some matching chars.
                 foreach (RouteSegment routeSegment in Children.ToList())
                 {
                     int index = 0;
                     while (IsCharAtEquals(routeSegment.UrlPart, url, index))
-                    {
                         index++;
-                    }
 
+                    // When whole segment url is matched, delegate inclusion to segment.
                     if(index == routeSegment.UrlPart.Length)
                     {
                         routeSegment.IncludeUrl(url, pipelineFactory);
                         return;
                     }
 
+                    // When at least one char is matched, then divide partialy-matched segment into to two with common parent.
                     if (index > 0)
                     {
-                        // Divide
+                        // Update structure so new segment has shared chars and partialy-matched segment is nested under.
                         Children.Remove(routeSegment);
                         StaticRouteSegment newRouteSegment = new StaticRouteSegment(routeSegment.UrlPart.Substring(0, index));
                         Children.Add(newRouteSegment);
                         newRouteSegment.Children.Add(routeSegment);
 
+                        // Remove shared chars from partialy-matched segment.
                         routeSegment.UrlPart = routeSegment.UrlPart.Substring(index);
 
+                        // Delegate inclusion to new segment.
                         newRouteSegment.IncludeUrl(url, pipelineFactory);
                         return;
                     }
                 }
 
+                // When any of children has same at least one char, include as new child-segment.
                 Children.Add(new StaticRouteSegment(url, pipelineFactory));
             }
         }
 
+        /// <summary>
+        /// Returns <c>true</c> when <paramref name="source"/> and <paramref name="target"/> at index <paramref name="index"/> has same chars.
+        /// When length of either source or target has length smaller then <paramref name="index"/>, returns <c>false</c>.
+        /// </summary>
+        /// <param name="source">First value.</param>
+        /// <param name="target">Second value.</param>
+        /// <param name="index">Index to compare <paramref name="source"/> and <paramref name="target"/> at.</param>
+        /// <returns><c>true</c> when <paramref name="source"/> and <paramref name="target"/> are equal at index <paramref name="index"/>.</returns>
         private bool IsCharAtEquals(string source, string target, int index)
         {
             if (source.Length <= index || target.Length <= index)
