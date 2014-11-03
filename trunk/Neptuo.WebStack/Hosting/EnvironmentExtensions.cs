3f0fda93d66fe6a1ea2da85d56e5bea4fb067a10
Mara <Mara@d3b2a240-04b9-5443-bd83-0f4699e8c419>
2014-11-03 21:27:02 +0000

    Extension to EngineEnvironment for root request handler.

--- trunk/Neptuo.WebStack/Hosting/EnvironmentExtensions.cs
+++ trunk/Neptuo.WebStack/Hosting/EnvironmentExtensions.cs
@@ -12,6 +12,34 @@ namespace Neptuo.WebStack.Hosting
     /// </summary>
     public static class EnvironmentExtensions
     {
+        #region Root handler
+
+        /// <summary>
+        /// Registers application root handler for HTTP request.
+        /// </summary>
+        /// <param name="environment">Engine environment.</param>
+        /// <param name="requestHandler">Handler for handling all requests.</param>
+        /// <returns><paramref name="environment"/>.</returns>
+        public static EngineEnvironment UseRootRequestHandler(this EngineEnvironment environment, IRequestHandler requestHandler)
+        {
+            Guard.NotNull(environment, "environment");
+            Guard.NotNull(requestHandler, "requestHandler");
+            return environment.Use<IRequestHandler>(requestHandler);
+        }
+
+        /// <summary>
+        /// Tries to retrieve root handler for HTTP request.
+        /// </summary>
+        /// <param name="environment">Engine environment.</param>
+        /// <returns>Root handler for HTTP request.</returns>
+        public static IRequestHandler WithRootRequestHandler(this EngineEnvironment environment)
+        {
+            Guard.NotNull(environment, "environment");
+            return environment.With<IRequestHandler>();
+        }
+
+        #endregion
+
         #region Route table
 
         /// <summary>
