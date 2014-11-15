803db5e9b7612c7421252ea82d724ac38bf307c7
Mara <Mara@d3b2a240-04b9-5443-bd83-0f4699e8c419>
2014-11-15 21:48:41 +0000

    UrlBuilder splitted into separated files. Some minor refactoring.

---
+++ trunk/Neptuo.WebStack.Http/IUrlDomainBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.WebStack.Http
+{
+    /// <summary>
+    /// Part of <see cref="IUrlBuilder"/> for building domain name of the URL.
+    /// </summary>
+    public interface IUrlDomainBuilder
+    {
+        /// <summary>
+        /// Sets domain name part of URL in the current builder.
+        /// </summary>
+        /// <param name="domain">Domain name.</param>
+        /// <returns>Path builder with domain name set to <paramref name="domain"/>.</returns>
+        IUrlPathBuilder Domain(string domain);
+    }
+}
