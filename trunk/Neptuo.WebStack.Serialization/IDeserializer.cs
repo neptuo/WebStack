edd07457e0ac2f65cde27b32647d03bed5fe1aad
Mara <Mara@d3b2a240-04b9-5443-bd83-0f4699e8c419>
2014-11-28 20:01:50 +0000

    Some basic stuff for serialization.

---
+++ trunk/Neptuo.WebStack.Serialization/IDeserializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.WebStack.Serialization
+{
+    /// <summary>
+    /// Contract for deserializing objects.
+    /// </summary>
+    public interface IDeserializer
+    {
+        /// <summary>
+        /// Deserializes object of type <paramref name="type"/> from <paramref name="stream"/>.
+        /// </summary>
+        /// <param name="stream">Input stream to read data from.</param>
+        /// <param name="type">Type of object to create.</param>
+        /// <returns>Instance of type <paramref name="type"/> created from <paramref name="stream"/>.</returns>
+        Task<object> DeserializeAsync(Stream stream, Type type);
+    }
+}
---
+++ trunk/Neptuo.WebStack.Serialization/ISerializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.WebStack.Serialization
+{
+    /// <summary>
+    /// Contract for serializing objects.
+    /// </summary>
+    public interface ISerializer
+    {
+        /// <summary>
+        /// Serializes <paramref name="instance"/> to the <paramref name="stream"/>.
+        /// </summary>
+        /// <param name="stream">Output stream.</param>
+        /// <param name="instance">Object to serialize.</param>
+        Task SerializeAsync(Stream stream, object instance);
+    }
+}
