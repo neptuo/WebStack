f4a11edcd3bb0281af81dd4b166ea64f3f2e8a79
Marek Fišera <maraf@users.noreply.github.com>
2014-11-28 20:01:50 +0000

    Some basic stuff for serialization.

---
+++ Neptuo.WebStack.Serialization/EnvironmentExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.WebStack.Serialization
+{
+    /// <summary>
+    /// Some extensions for <see cref="EngineEnvironment"/>.
+    /// </summary>
+    public static class EnvironmentExtensions
+    {
+        /// <summary>
+        /// Registers serializer and deserializer collection and uses <paramref name="mapper"/> to register serializers and deserializers.
+        /// </summary>
+        /// <param name="environment">Engine environment.</param>
+        /// <param name="mapper">Mapper function for registering serializers and deserializers.</param>
+        /// <returns><paramref name="environment"/>.</returns>
+        public static EngineEnvironment UseSerialization(this EngineEnvironment environment, Action<ISerializerCollection, IDeserializerCollection> mapper)
+        {
+            Guard.NotNull(environment, "environment");
+            Guard.NotNull(mapper, "mapper");
+            return environment;
+        }
+
+        /// <summary>
+        /// Tries to retrieve <see cref="ISerializerCollection"/> from <paramref name="environment"/>.
+        /// </summary>
+        /// <param name="environment">Engine environment.</param>
+        /// <returns>Registered <see cref="ISerializerCollection"/>.</returns>
+        public static ISerializerCollection WithSerializerCollection(this EngineEnvironment environment)
+        {
+            Guard.NotNull(environment, "environment");
+            return environment.With<ISerializerCollection>();
+        }
+
+        /// <summary>
+        /// Tries to retrieve <see cref="IDeserializerCollection"/> from <paramref name="environment"/>.
+        /// </summary>
+        /// <param name="environment">Engine environment.</param>
+        /// <returns>Registered <see cref="IDeserializerCollection"/>.</returns>
+        public static IDeserializerCollection WithDeserializerCollection(this EngineEnvironment environment)
+        {
+            Guard.NotNull(environment, "environment");
+            return environment.With<IDeserializerCollection>();
+        }
+    }
+}
---
+++ Neptuo.WebStack.Serialization/ISerializerCollection.cs
@@ -0,0 +1,31 @@
+﻿using Neptuo.WebStack.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.WebStack.Serialization
+{
+    /// <summary>
+    /// Collection of registered deserializers.
+    /// </summary>
+    public interface ISerializerCollection
+    {
+        /// <summary>
+        /// Mapps <paramref name="serializer"/> for serializing when required content type is <paramref name="contentType"/>.
+        /// </summary>
+        /// <param name="contentType">Content type for which <paramref name="serializer"/> will be used.</param>
+        /// <param name="serializer">Serializer to use for requests with content type <paramref name="contentType"/>.</param>
+        /// <returns>Self (for fluency).</returns>
+        ISerializerCollection Map(HttpMediaType contentType, ISerializer serializer);
+
+        /// <summary>
+        /// Tries to find serializer for <paramref name="contentType"/>.
+        /// </summary>
+        /// <param name="contentType">Content type for which to return the serializer.</param>
+        /// <param name="serializer">Serializer mapped to <paramref name="contentType"/>.</param>
+        /// <returns><c>true</c> if such serializer exists; <c>false</c> otherwise.</returns>
+        bool TryGet(HttpMediaType contentType, out ISerializer serializer);
+    }
+}
