d441fbedef29338922500e76c97e7921d0f51082
Mara <Mara@d3b2a240-04b9-5443-bd83-0f4699e8c419>
2014-12-11 11:55:23 +0000

    Serialization and deserialization improvements.

--- trunk/Neptuo.WebStack.Serialization/IDeserializer.cs
+++ trunk/Neptuo.WebStack.Serialization/IDeserializer.cs
@@ -14,10 +14,11 @@ namespace Neptuo.WebStack.Serialization
     {
         /// <summary>
         /// Deserializes object of type <paramref name="type"/> from <paramref name="stream"/>.
+        /// If returns <c>null</c>, deserialization was not possible.
         /// </summary>
         /// <param name="stream">Input stream to read data from.</param>
         /// <param name="type">Type of object to create.</param>
         /// <returns>Instance of type <paramref name="type"/> created from <paramref name="stream"/>.</returns>
-        Task<object> DeserializeAsync(Stream stream, Type type);
+        Task<object> TryDeserializeAsync(Stream stream, Type type);
     }
 }
---
+++ trunk/Neptuo.WebStack.Serialization/_SerializationExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Neptuo.WebStack.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.WebStack.Serialization
+{
+    /// <summary>
+    /// Common extensions for serializing and deserialiting objects.
+    /// </summary>
+    public static class _SerializationExtensions
+    {
+        public static async Task<T> TryDeserialize<T>(this IDeserializerCollection collection, HttpMediaType dataType, Stream rawData)
+        {
+            Guard.NotNull(collection, "collection");
+
+            IDeserializer deserializer;
+            if (!collection.TryGet(dataType, out deserializer))
+                return default(T);
+
+            return (T)(await deserializer.TryDeserializeAsync(rawData, typeof(T)));
+        }
+
+        public static async Task<bool> TrySerialize<T>(this ISerializerCollection collection, HttpMediaType dataType, Stream target, T instance)
+        {
+            Guard.NotNull(collection, "collection");
+
+            ISerializer serializer;
+            if (!collection.TryGet(dataType, out serializer))
+                return false;
+
+            return await serializer.TrySerializeAsync(target, instance);
+        }
+    }
+}
