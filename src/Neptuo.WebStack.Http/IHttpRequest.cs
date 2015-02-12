5e8d2326ee942c69d9c9fec905dfc390ef6ee42b
maraf <mara@neptuo.com>
2015-02-12 09:57:43 +0100

    Move URL classes to separate folder (not namespace). Http request raw values.

--- src/Neptuo.WebStack.Http/IHttpRequest.cs
+++ src/Neptuo.WebStack.Http/IHttpRequest.cs
@@ -13,60 +13,14 @@ namespace Neptuo.WebStack.Http
     public interface IHttpRequest
     {
         /// <summary>
-        /// Current service container.
+        /// Raw HTTP request values.
         /// </summary>
-        IDependencyProvider DependencyProvider();
-
-        /// <summary>
-        /// Http method.
-        /// </summary>
-        HttpMethod Method();
-
-        /// <summary>
-        /// Requested url.
-        /// </summary>
-        IReadOnlyUrl Url();
-
-        /// <summary>
-        /// Http request headers.
-        /// </summary>
-        IReadOnlyKeyValueCollection Headers();
-
-        /// <summary>
-        /// Input query string.
-        /// </summary>
-        IHttpParamCollection QueryString();
-
-        /// <summary>
-        /// Input stream.
-        /// </summary>
-        Stream InputStream();
-
-        /// <summary>
-        /// Data posted as form data.
-        /// </summary>
-        IHttpParamCollection Form();
-
-        /// <summary>
-        /// Posted files.
-        /// </summary>
-        IEnumerable<IHttpFile> Files();
-
-        /// <summary>
-        /// Cancellation token indicating whether the HTTP request was cancelled.
-        /// </summary>
-        CancellationToken CancellationToken();
-
-        /// <summary>
-        /// Event fired when disposing this HTTP request object.
-        /// </summary>
-        event Action OnDisposing;
+        IRawHttpRequest RawValues { get; }
 
         /// <summary>
         /// Collection of supported values.
         /// </summary>
         [EditorBrowsable(EditorBrowsableState.Never)]
-        IKeyValueCollection CustomValues();
+        IKeyValueCollection CustomValues { get; }
     }
-
 }
---
+++ src/Neptuo.WebStack.Http/IRawHttpRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.WebStack.Http
+{
+    /// <summary>
+    /// Base HTTP request description.
+    /// </summary>
+    public interface IRawHttpRequest
+    {
+        /// <summary>
+        /// HTTP method used in request.
+        /// </summary>
+        string Method { get; }
+
+        /// <summary>
+        /// Raw URL (url + query string as client requested).
+        /// May contain also schema and domain.
+        /// </summary>
+        string Url { get; }
+
+        /// <summary>
+        /// HTTP protocol used in request.
+        /// Eg. HTTP/1.1
+        /// </summary>
+        string Protocol { get; }
+
+        /// <summary>
+        /// Key-value collection to request headers.
+        /// </summary>
+        IDictionary<string, string> Headers { get; }
+
+        /// <summary>
+        /// Raw request body.
+        /// </summary>
+        Stream InputStream { get; }
+    }
+}
