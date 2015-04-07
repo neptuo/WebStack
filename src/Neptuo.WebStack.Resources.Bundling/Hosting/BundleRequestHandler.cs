46c16af626bac89bf9b75336b0901f5410c0818b
maraf <mara@neptuo.com>
2015-04-07 12:24:59 +0200

    Preparation for hosting bundles.

---
+++ src/Neptuo.WebStack.Resources.Bundling/Hosting/BundleRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using Neptuo.WebStack.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Optimization;
+
+namespace Neptuo.WebStack.Resources.Bundling.Hosting
+{
+    /// <summary>
+    /// Request handler for downloading bundles.
+    /// </summary>
+    public class BundleRequestHandler : IRequestHandler
+    {
+        public Task<bool> TryHandleAsync(IHttpContext httpContext)
+        {
+            Bundle bundle;
+            if(httpContext.CustomValues().TryGetBundle(out bundle))
+            {
+                //TODO: Create bundle context, so BundleResponse can be created.
+                BundleResponse response = null; throw Ensure.Exception.NotImplemented(); // bundle.GenerateBundleResponse(new BundleContext());
+                httpContext.Response().OutputWriter().Write(response.Content);
+                httpContext.Response().Headers().ContentType(new HttpMediaType(response.ContentType));
+
+                return Task.FromResult(true);
+            }
+
+            return Task.FromResult(false);
+        }
+    }
+}
---
+++ src/Neptuo.WebStack.Resources.Bundling/Hosting/_KeyValueCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Neptuo.Collections.Specialized;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Optimization;
+
+namespace Neptuo.WebStack.Resources.Bundling.Hosting
+{
+    /// <summary>
+    /// Bundle extensions for <see cref="IKeyValueCollection"/>.
+    /// </summary>
+    public static class _KeyValueCollectionExtensions
+    {
+        public static IKeyValueCollection Bundle(this IKeyValueCollection collection, Bundle bundle)
+        {
+            Ensure.NotNull(collection, "collection");
+            return collection.Set("Bundle", bundle);
+        }
+
+        public static bool TryGetBundle(this IReadOnlyKeyValueCollection collection, out Bundle bundle)
+        {
+            Ensure.NotNull(collection, "collection");
+            return collection.TryGet("Bundle", out bundle);
+        }
+    }
+}
